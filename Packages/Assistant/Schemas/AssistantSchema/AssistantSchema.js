45866cdc4c378aa93a73238e6534953a635dfe36
alexbondj <alexbondj@gmail.com>
2017-04-27 20:48:11 +0300

    Add containerlisk and buttons

--- Packages/Assistant/Schemas/AssistantSchema/AssistantSchema.js
+++ Packages/Assistant/Schemas/AssistantSchema/AssistantSchema.js
@@ -6,14 +6,71 @@ define("AssistantSchema", ["AssistantSchemaResources"],
             mixins: {
              },
             attributes: {
+                "ActionCollection": {
+                    dataValueType: this.Terrasoft.DataValueType.COLLECTION
+                }
             },
             methods: {
+
+                onGetItemConfig: function(itemConfig) {
+                    var viewConfig = this.Terrasoft.deepClone(this.get("EmailViewConfig"));
+                    itemConfig.config = viewConfig;
+                },
 
                 /**
                  * Initializes the schema.
                  */
-                init: function() {
-                    this.callParent(arguments);
+                init: function(callback) {
+                    this.initParameters();
+                    this.callParent([function() {
+                        Terrasoft.chain(
+                            //this.initEmailTabActionsMenuCollection,
+                            //this.initEmailProcessTag,
+                            function() {
+                                this.loadData();
+                                callback();
+                            }, this);
+                    }, this]);
+                },
+
+                initParameters: function() {
+                    this.set("ActionCollection", this.Ext.create("Terrasoft.BaseViewModelCollection"));
+                },
+
+                /**
+                 * Load data
+                 * @protected
+                 * @param {Boolean} clearCollection Clear existing emails list flag.
+                 */
+                loadData: function(clearCollection) {
+                    var config = {
+                        serviceName: "QuartzSchedulerProxyService",
+                        methodName: "GetJobInfo",
+                        scope: this,
+                        data: {}
+                    };
+                    this.callService(config, this.onActionLoaded, this);
+                },
+
+                reload: function() {
+                    this.loadData();
+                },
+
+                onActionLoaded: function(result) {
+                    if (result.success) {
+                        var dataCollection = result.collection;
+                        //this.set("CanLoadMoreData", dataCollection.getCount() > 0);
+                        var data = this.Ext.create("Terrasoft.BaseViewModelCollection");
+                        dataCollection.each(function(item) {
+                            //var model = this.onLoadEntity(item);
+                            data.add(item.get("Id"), model);
+                        }, this);
+                        var collection = this.get("ActionCollection");
+                        collection.clear();
+                        collection.loadAll(data);
+                    }
+                    this.hideBodyMask();
+                    this.set("IsDataLoaded", true);
                 },
 
                 /**
@@ -25,15 +82,168 @@ define("AssistantSchema", ["AssistantSchemaResources"],
             diff: [
                 {
                     "operation": "insert",
-                    "name": "assistantMainContainer",
+                    "name": "AssistantMainContainer",
                     "values": {
-                        "id": "assistantMainContainer",
+                        "id": "AssistantMainContainer",
                         "selectors": {"wrapEl": "#assistantMainContainer"},
                         "itemType": Terrasoft.ViewItemType.CONTAINER,
                         "wrapClass": ["assistantMainContainer"],
                         "items": []
                     }
+                },
+                {
+                    "operation": "insert",
+                    "name": "AssistantTabHeader",
+                    "propertyName": "items",
+                    "parentName": "AssistantMainContainer",
+                    "values": {
+                        "itemType": Terrasoft.ViewItemType.CONTAINER,
+                       // "classes": {"wrapClassName": ["emails-header-container"]},
+                        "items": []
+                    }
+                },
+                {
+                    "operation": "insert",
+                    "parentName": "AssistantTabHeader",
+                    "propertyName": "items",
+                    "name": "AssistantButton",
+                    "values": {
+                        "itemType": Terrasoft.ViewItemType.BUTTON,
+                        "caption": "Add Action",
+                        //"caption": {
+                        //    "bindTo": "getMailTypeCaption"
+                        //},
+                        "style": Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                        //"classes": {
+                        //    "wrapperClass": ["email-type-button-wrapper", "left-button"],
+                        //    "menuClass": ["email-type-button-menu"]
+                        //},
+                        "menu": {
+                            "items": {"bindTo": "getAssistantMenuItems"}
+                        },
+                        "markerValue": "AssistantButton"
+                    }
+                },
+                {
+                    "operation": "insert",
+                    "name": "AddAction",
+                    "propertyName": "items",
+                    "parentName": "AssistantTabHeader",
+                    "values": {
+                        "itemType": Terrasoft.ViewItemType.BUTTON,
+                        "imageConfig": {"bindTo": "Resources.Images.AddActionImage"},
+                        "style": Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                        "click": {"bindTo": "addActon"}//,
+                        //"classes": {wrapClassName: ["add-email-button-wrap"]}
+                    }
+                },
+                {
+                    "operation": "insert",
+                    "name": "Reload",
+                    "propertyName": "items",
+                    "parentName": "AssistantTabHeader",
+                    "values": {
+                        "itemType": Terrasoft.ViewItemType.BUTTON,
+                        "imageConfig": {"bindTo": "Resources.Images.ReloadImage"},
+                        "style": Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                        "click": {"bindTo": "reload"}//,
+                        //"classes": {wrapClassName: ["add-email-button-wrap"]}
+                    }
+                },
+                {
+                    "operation": "insert",
+                    "name": "ActionTabActions",
+                    "propertyName": "items",
+                    "parentName": "AssistantTabHeader",
+                    "values": {
+                        "itemType": Terrasoft.ViewItemType.BUTTON,
+                        "imageConfig": {"bindTo": "Resources.Images.ActionsButtonImage"},
+                        "style": Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                        //"classes": {
+                        //    //wrapperClass: ["email-actions-button-wrapper", "email-tab-actions-button-wrapper"],
+                        //    //menuClass: ["email-actions-button-menu"]
+                        //},
+                        "controlConfig": {
+                            "menu": {
+                                "items": {"bindTo": "ActionTabActionsMenuCollection"}
+                            }
+                        },
+                        //"visible": {"bindTo": "IsEmailTabActionsVisible"},
+                        "click": {"bindTo": "onActionsClick"},
+                        "markerValue": "EmailTabActions",
+                        "tips": []
+                    }
+                },
+                //{
+                //    "operation": "insert",
+                //    "parentName": "EmailTabActions",
+                //    "propertyName": "tips",
+                //    "name": "EmailTabActionsTooltip",
+                //    "values": {
+                //        "content": {"bindTo": "TextEmailTabActionsTooltip"},
+                //        "visible": {"bindTo": "ShowEmailTabActionsTooltip"},
+                //        "linkClicked": {"bindTo": "onEmailTabActionsTooltipClick"},
+                //        "items": [],
+                //        "behaviour": {
+                //            "displayEvent": Terrasoft.TipDisplayEvent.NONE
+                //        },
+                //        "restrictAlignType": Terrasoft.AlignType.BOTTOM
+                //    }
+                //},
+                {
+                    "operation": "insert",
+                    "name": "ActionContainerList",
+                    "propertyName": "items",
+                    "parentName": "AssistantMainContainer",
+                    "values": {
+                        "itemType": Terrasoft.ViewItemType.CONTAINER,
+                        "generator": "ContainerListGenerator.generateGrid",
+                        "collection": {"bindTo": "ActionCollection"},
+                        //"classes": {"wrapClassName": ["emails-container-list"]},
+                        "onGetItemConfig": {"bindTo": "onGetItemConfig"},
+                        "idProperty": "Id",
+                        "observableRowNumber": 1,
+                        "observableRowVisible": {"bindTo": "onLoadNext"},
+                        //"rowCssSelector": ".email-container.selectable",
+                        "getEmptyMessageConfig": {bindTo: "getEmptyMessageConfig"},
+                        "items": []
+                    }
                 }
+                //,
+                //{
+                //    "operation": "insert",
+                //    "name": "NewButtonContainer",
+                //    "propertyName": "items",
+                //    "parentName": "EmailTabHeader",
+                //    "values": {
+                //        "itemType": Terrasoft.ViewItemType.CONTAINER,
+                //        "classes": {"wrapClassName": ["email-reload-panel-button-wrapper"]},
+                //        "visible": {
+                //            "bindTo": "NewEmailsCounter",
+                //            "bindConfig": {"converter": "getNewEmailsButtonVisible"}
+                //        },
+                //        "items": []
+                //    }
+                //},
+                //{
+                //    "operation": "insert",
+                //    "name": "NewEmailsButton",
+                //    "parentName": "NewEmailsButtonContainer",
+                //    "propertyName": "items",
+                //    "values": {
+                //        "itemType": Terrasoft.ViewItemType.BUTTON,
+                //        "imageConfig": {"bindTo": "Resources.Images.More"},
+                //        "caption": {
+                //            "bindTo": "NewEmailsCounter",
+                //            "bindConfig": {"converter": "getNewEmailsButtonCaption"}
+                //        },
+                //        "style": Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                //        "click": {"bindTo": "reloadEmails"},
+                //        "markerValue": "LoadNewEmails",
+                //        "tips": []
+                //    }
+                //
+                //}
 
             ]
         };
